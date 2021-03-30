72be000a78b628cd87dd524634d97d7b731a29ee
bockellt <64434783+bockellt@users.noreply.github.com>
2021-03-30 16:41:12 -0400

    Add files via upload

---
+++ src/User.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author laurenbockelman
+ * 
+ * Admin Class for EASE Application
+ * 
+ * Copyright (C) 2021 bockellt@maimoh.edu
+ *
+ */
+public class User {
+	// instance variable
+	boolean isAdmin = true;
+	
+	
+
+}
