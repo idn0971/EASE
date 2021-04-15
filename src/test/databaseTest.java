985187dee266ef0095ead0040a28b96fcc7d6262
idn0971 <idn0971@gmail.com>
2021-04-15 17:30:43 -0400

    Someone broke the tests

---
+++ src/test/databaseTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import main.Application;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+class DatabaseTest {
+    @Test
+    void getAndSetName() {
+
+    }
+}
